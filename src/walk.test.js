18e46c905bd4ead4c6dcc1eebdaaf06f713232ef
Daniel D. Beck <daniel@ddbeck.com>
2021-02-16 16:25:23 +0000

    Add walk function

---
+++ src/walk.test.js
@@ -0,0 +1,24 @@
+const assert = require("assert");
+
+const { walk } = require("./index");
+
+describe("walk", function () {
+  it("should walk a single tree", function () {
+    let results = Array.from(walk("api.Notification"));
+    assert.strictEqual(results.length, 27);
+    assert.strictEqual(results[0].path, "api.Notification");
+    assert.strictEqual(results[1].path, "api.Notification.Notification");
+  });
+
+  it("should walk multiple trees", function () {
+    let results = Array.from(
+      walk(["api.Notification", "css.properties.color"])
+    );
+    assert.strictEqual(results.length, 28);
+    assert.strictEqual(results[0].path, "api.Notification");
+    assert.strictEqual(
+      results[results.length - 1].path,
+      "css.properties.color"
+    );
+  });
+});
