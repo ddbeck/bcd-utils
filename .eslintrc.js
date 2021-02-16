4ccd6a2090a0b2740fa2c0229e0f9c668cec5320
Daniel D. Beck <daniel@ddbeck.com>
2021-02-16 13:26:44 +0000

    Add Mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    mocha: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
 };
