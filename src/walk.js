6268379e55b697bca4e22ba26a9eb5182de6c320
Daniel D. Beck <daniel@ddbeck.com>
2021-02-26 18:09:52 +0000

    Rewrite walk so it can be controlled from outside

--- src/walk.js
+++ src/walk.js
@@ -1,28 +1,17 @@
 const bcd = require("./bcd");
 const query = require("./query");
 
-const walkableBcdKeys = [...Object.keys(bcd).filter((k) => k !== "browsers")];
+const topLevelBcdKeys = Object.keys(bcd);
+const topLevelBcdKeysWalkable = [
+  ...Object.keys(bcd).filter((k) => k !== "browsers"),
+];
 
-function* walk(startingPoints = walkableBcdKeys, data = bcd) {
-  // Convert string to array of strings
-  if (!Array.isArray(startingPoints)) {
-    startingPoints = [startingPoints];
-  }
+function isTopLevelBcd(obj) {
+  return topLevelBcdKeys.every((key) => key in obj);
+}
 
-  for (const start of startingPoints) {
-    const ns = query(start, data);
-
-    if (isFeature(ns)) {
-      yield {
-        path: start,
-        feature: ns.__compat,
-      };
-    }
-
-    for (const key of descendantKeys(ns)) {
-      yield* walk(`${start}.${key}`, data);
-    }
-  }
+function joinPath() {
+  return Array.from(arguments).filter(Boolean).join(".");
 }
 
 function* descendantKeys(data) {
@@ -35,6 +24,46 @@ function* descendantKeys(data) {
 
 function isFeature(obj) {
   return "__compat" in obj;
+}
+
+function* lowLevelWalk(data = bcd, path, depth = Infinity) {
+  if (isFeature(data)) {
+    yield {
+      path: path,
+      compat: data.__compat,
+    };
+  }
+
+  let childKeys = descendantKeys(data);
+  if (path === undefined && isTopLevelBcd(data)) {
+    childKeys = topLevelBcdKeysWalkable;
+  }
+
+  if (depth === 0) {
+    return;
+  }
+
+  for (const key of childKeys) {
+    yield* lowLevelWalk(data[key], joinPath(path, key), depth - 1);
+  }
+}
+
+/**
+ * Walk the tree of compat features from one or more starting points. Yields the path to each feature and the inner compat object.
+ *
+ * @param {string[]} [entryPoints] One or more dotted paths to traverse the tree from.
+ * @yields {{path: string, compat: Object}} A path and `__compat` object
+ */
+function* walk(entryPoints) {
+  if (entryPoints === undefined) {
+    yield* lowLevelWalk();
+  } else {
+    entryPoints = Array.isArray(entryPoints) ? entryPoints : [entryPoints];
+
+    for (const entryPoint of entryPoints) {
+      yield* lowLevelWalk(query(entryPoint), entryPoint);
+    }
+  }
 }
 
 module.exports = walk;
