4f2499c81cd3c683b034f53e3176e9289460379b
Daniel D. Beck <daniel@ddbeck.com>
2021-03-05 19:31:00 +0000

    Add iterSupport

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const iterSupport = require("./iter-support");
 const query = require("./query");
 const { walk } = require("./walk");
 const visit = require("./visit");
 
 module.exports = {
+  iterSupport,
   query,
   walk,
   visit,
