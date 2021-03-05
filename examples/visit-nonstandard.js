3a1f1c2fffdbce6f7e4718e3ff9e2d32dc2bfc8c
Daniel D. Beck <daniel@ddbeck.com>
2021-03-05 19:12:48 +0000

    Add another visit example

---
+++ examples/visit-nonstandard.js
@@ -0,0 +1,12 @@
+const { visit } = require("../src/index.js");
+
+visit(
+  undefined,
+  (path) => {
+    return path.startsWith("api.RTC");
+  },
+  (path, feature) => {
+    const std = feature.status.standard_track ? "✅" : "❗️";
+    console.log(`${std} ${path}`);
+  }
+);
