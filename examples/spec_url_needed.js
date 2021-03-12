77f625172c06506514e6d03b3b5025893587c503
Daniel D. Beck <daniel@ddbeck.com>
2021-03-12 21:04:22 +0000

    Break up spec_url_needed by milestone

--- examples/spec_url_needed.js
+++ examples/spec_url_needed.js
@@ -1,15 +1,47 @@
 const { visit } = require('..');
 
-visit(
-  undefined,
-  () => true,
-  (path, compat) => {
-    if (
-      compat.status !== undefined &&
-      compat.status.standard_track === true &&
-      compat.spec_url === undefined
-    ) {
-      console.log(path);
-    }
-  },
-);
+function hasMdnUrl(compat) {
+  return compat.mdn_url !== undefined;
+}
+
+function isStandardTrack(path, compat) {
+  return compat.status !== undefined && compat.status.standard_track === true;
+}
+
+function missingSpecUrl(compat) {
+  return compat.spec_url === undefined;
+}
+
+const standardTrack = new Set();
+const missingSpec = new Set();
+
+const milestone1 = new Set();
+const milestone2 = new Set();
+
+visit(undefined, isStandardTrack, (path, compat) => {
+  standardTrack.add(path);
+  if (hasMdnUrl(compat)) {
+    milestone1.add(path);
+  } else {
+    milestone2.add(path);
+  }
+
+  if (missingSpecUrl(compat)) {
+    missingSpec.add(path);
+  }
+});
+
+console.log('## Milestone 1');
+for (const path of standardTrack) {
+  if (missingSpec.has(path) && milestone1.has(path)) {
+    console.log(`1. \`${path}\``);
+  }
+}
+console.log();
+
+console.log('## Milestone 2');
+for (const path of standardTrack) {
+  if (missingSpec.has(path) && milestone2.has(path)) {
+    console.log(`1. \`${path}\``);
+  }
+}
