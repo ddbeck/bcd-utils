699c958094a0c87f4741e7fb18765883794865b8
Daniel D. Beck <daniel@ddbeck.com>
2023-01-25 15:52:11 +0100

    Convert

--- src/query.ts
+++ src/query.ts
@@ -8,16 +8,20 @@ import bcd from '@mdn/browser-compat-data' assert { type: 'json' };
  * @returns {*} A BCD subtree
  * @throws {ReferenceError} For invalid identifiers
  */
-function query(path, data = bcd) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function query(path: string, data: Record<string, any> = bcd) {
   const pathElements = path.split('.');
   let lookup = data;
+
   while (pathElements.length) {
     const next = pathElements.shift();
-    lookup = lookup[next];
-    if (lookup === undefined) {
+
+    if (typeof next === 'undefined' || !(next in lookup)) {
       throw new ReferenceError(
         `${path} is not a valid tree identifier (failed at '${next}')`,
       );
+    } else {
+      lookup = lookup[next];
     }
   }
   return lookup;
